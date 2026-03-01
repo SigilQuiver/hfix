0b51364d9e717772f3aec0ca6d3ebd39eb8ee4bf
SigilQuiver <75144301+SigilQuiver@users.noreply.github.com>
2026-03-01 00:51:30 +0000

    created new api that scrapes

---
+++ index.js
@@ -0,0 +1,86 @@
+const cheerio = require("cheerio");
+const fs = require('node:fs');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+// scrape video data from pmvhaven
+async function get_video(url) {
+
+  // get html
+  const res2 = await fetch(url);
+  const html = await res2.text();
+
+  // parse
+  const $ = cheerio.load(html);
+
+  //check all scripts that have json type in html head
+  for (const el of $('script[type="application/ld+json"]',"head")){
+    try{
+      // get script content
+      let d = el.children[0].data;
+      // parse content into json
+      d = JSON.parse(d);
+      // see is json type is video
+      if (d["@type"] == "VideoObject"){
+        //return valid data
+        return d;
+      }
+    } catch (e){
+
+    }
+  }
+  return null;
+}
+
+app.get("/favicon.ico",(req,res) =>{
+  res.end();
+});
+
+//read end of url
+app.get("/{*splat}",async (req,res) =>{
+
+  //add url to original url
+  const url = "https://pmvhaven.com"+req.originalUrl;
+  console.log("looking at:",url);
+
+  // get video data
+  const video_data = await get_video(url);
+
+  // get html file to string
+  var html;
+  html = fs.readFileSync('./og.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    html = data;
+
+
+    return  html
+  });
+  console.log(Object.keys(video_data))
+
+  //add in ogl data
+  html = html.replaceAll("!url",video_data.embedUrl);
+  html = html.replaceAll("!title",video_data.name);
+  html = html.replaceAll("!description",video_data.description);
+  let video_url = video_data.contentUrl;
+  video_url = video_url.substring(0,video_url.lastIndexOf("/"));
+  html = html.replaceAll("!video",video_url);
+  console.log("content_url:"+video_url);
+
+  //send response
+  res.set('Content-Type', 'text/html');
+  res.send(Buffer.from(html));
+
+});
+
+//listen to url, show url
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/video/1-the-milf-show-with-moans_696bf6e2c8a41df7bc3bfa3e?from=recommended`);
+}); 
+
