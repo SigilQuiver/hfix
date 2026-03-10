1d11146c8a4cfca2028d8b717a48f3dda1e367f7
SigilQuiver <75144301+SigilQuiver@users.noreply.github.com>
2026-03-10 00:45:27 +0000

    changed cache install path

--- render-build.sh
+++ render-build.sh
@@ -22,5 +22,5 @@ echo "...Copying Puppeteer Cache from Build Cache"
 cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
 else
 echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome
 fi
