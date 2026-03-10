262d84e5ca0d2f3fe33694cde51b1c2d8ca817d6
SigilQuiver <75144301+SigilQuiver@users.noreply.github.com>
2026-03-10 01:21:31 +0000

    used move command instead of copy

--- render-build.sh
+++ render-build.sh
@@ -9,18 +9,11 @@ npm install
 #npm run build
 #Ensure the Puppeteer cache directory exists
 
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 #Install Puppeteer and download Chrome
 
 npx puppeteer browsers install chrome
-#Store/pull Puppeteer cache with build cache
 
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-echo "...Copying Puppeteer Cache from Build Cache"
-# Copying from the actual path where Puppeteer stores its Chrome binary
-cp -R /opt/render/project/src/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome $PUPPETEER_CACHE_DIR
-else
-echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome
-fi
+#Move directory where puppeteer cache is
+mv /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
