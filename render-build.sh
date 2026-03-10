8ee5f1609a3645915c9a31c6a3f5674881116f74
SigilQuiver <75144301+SigilQuiver@users.noreply.github.com>
2026-03-10 01:24:31 +0000

    reverted to copy, changed to different directory

--- render-build.sh
+++ render-build.sh
@@ -15,5 +15,13 @@ mkdir -p $PUPPETEER_CACHE_DIR
 
 npx puppeteer browsers install chrome
 
-#Move directory where puppeteer cache is
-mv /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+
+#Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+echo "...Copying Puppeteer Cache from Build Cache"
+# Copying from the actual path where Puppeteer stores its Chrome binary
+cp -R /opt/render/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+else
+echo "...Storing Puppeteer Cache in Build Cache"
+cp -R $PUPPETEER_CACHE_DIR /opt/render/.cache/puppeteer/chrome/
+fi
